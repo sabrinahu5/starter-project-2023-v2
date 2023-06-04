8f1693063d51be0a575c58921af3b1cdb873652e
Matthew Su <matthewsu@college.harvard.edu>
2023-06-04 16:51:58 -0500

    removed auth helpers react and auth ui react logic

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,18 +1,7 @@
 "use client";
 
-import { type Database } from "@/lib/schema";
-import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
-import { SessionContextProvider } from "@supabase/auth-helpers-react";
 import { ThemeProvider } from "next-themes";
-import { useState } from "react";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  // Create a new supabase client on every first-render only
-  const [supabaseClient] = useState(() => createClientComponentClient<Database>());
-
-  return (
-    <SessionContextProvider supabaseClient={supabaseClient}>
-      <ThemeProvider>{children}</ThemeProvider>
-    </SessionContextProvider>
-  );
+  return <ThemeProvider>{children}</ThemeProvider>;
 }
