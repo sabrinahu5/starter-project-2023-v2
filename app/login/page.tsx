8a5d2b9a1b66d4329da8b3e8ce4c25f6809ecd8f
Matthew Su <matthewsu@college.harvard.edu>
2023-07-07 13:48:26 -0400

    remove delay, redirect authenticated users away from /login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,22 @@
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { UserAuthForm } from "./user-auth-form";
+import { type Database } from "@/lib/schema";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
-export default function LoginPage() {
+export default async function LoginPage() {
+
+  // Create supabase server component client and obtain user session from stored cookie
+  const supabase = createServerComponentClient<Database>({ cookies });
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  if (session) {
+    // Users who are already signed in should be redirected to dashboard
+    redirect("/dashboard");
+  }
+
   return (
     <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
       <div className="flex flex-col space-y-2 text-center">
