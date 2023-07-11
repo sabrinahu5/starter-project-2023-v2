8ea9d339c9eca60c25b14e98d72c6c1aca080348
Jackson <jackson@jacksonmoody.com>
2023-07-10 23:06:27 -0500

    Added image component

--- next.config.js
+++ next.config.js
@@ -4,6 +4,20 @@ await import("./env.mjs");
 
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "upload.wikimedia.org",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "raw.githubusercontent.com",
+        port: "",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
