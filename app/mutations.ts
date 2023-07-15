853de195fd1b41d84f8d463419d250638837c473
Jackson <jackson@jacksonmoody.com>
2023-07-15 01:25:35 -0600

    Extracted mutations and types

---
+++ app/mutations.ts
@@ -0,0 +1,23 @@
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { type Database } from "@/lib/schema";
+
+type NewEntry = Database["public"]["Tables"]["species"]["Insert"];
+
+export async function addEntry(input: NewEntry) {
+  const supabase = createClientComponentClient<Database>();
+  await supabase
+      .from("species")
+      .insert([
+        {
+          author: input.author,
+          common_name: input.common_name,
+          continents: input.continents,
+          description: input.description,
+          kingdom: input.kingdom,
+          oceans: input.oceans,
+          scientific_name: input.scientific_name,
+          total_population: input.total_population,
+          image: input.image,
+        },
+      ]);
+}
---
+++ lib/types.ts
@@ -0,0 +1,22 @@
+import { z } from "zod";
+
+export const continents = ["Africa", "Antarctica","Asia","Australia","Europe","North America","South America"] as const;
+export const kingdoms = ["Animalia", "Plantae", "Fungi", "Protista", "Archaea", "Bacteria"] as const;
+export const oceans = ["Pacific", "Atlantic", "Indian", "Arctic", "Southern"] as const;
+
+export const continentOptions = continents.map(val => ({ value: val, label: val }));
+export const oceanOptions = oceans.map(val => ({ value: val, label: val }));
+export const kingdomOptions = kingdoms.map(val => ({ value: val, label: val }));
+
+export const speciesSchema = z.object({
+  common_name: z.string().optional(),
+  continents: z.array(z.enum(continents)).optional(),
+  description: z.string().optional(),
+  kingdom: z.enum(kingdoms),
+  oceans: z.array(z.enum(oceans)).optional(),
+  scientific_name: z.string().min(1),
+  total_population: z.number().int().positive().min(1).optional(),
+  image: z.string().url().optional().or(z.literal("")),
+});
+
+export type FormData = z.infer<typeof speciesSchema>;
