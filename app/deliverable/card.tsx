68fa6a8fc2bdb616ef81af9a86d36467275d15de
Jackson <jackson@jacksonmoody.com>
2023-07-14 19:45:49 -0600

    Added support for unoptimized images

--- app/deliverable/card.tsx
+++ app/deliverable/card.tsx
@@ -6,7 +6,9 @@ type Species = Database["public"]["Tables"]["species"]["Row"];
 export default function Card(species: Species) {
   return (
     <div className="flex-none m-4 w-72 min-w-72 rounded border-2 p-3 shadow">
-      {species.image && <Image src={species.image} width={300} height={100} alt={species.scientific_name} priority/>}
+      {species.image && <div className="relative w-full h-40">
+        <Image src={species.image} alt={species.scientific_name} fill style={{ objectFit: "cover" }}/>
+      </div>}
       <h3 className="mt-3 text-2xl font-semibold">{species.common_name}</h3>
       <h4 className="text-lg font-light italic">{species.scientific_name}</h4>
       <p>{species.description}</p>
--- next.config.js
+++ next.config.js
@@ -5,23 +5,7 @@ await import("./env.mjs");
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "upload.wikimedia.org",
-        port: "",
-      },
-      {
-        protocol: "https",
-        hostname: "raw.githubusercontent.com",
-        port: "",
-      },
-      {
-        protocol: "https",
-        hostname: "en.wikipedia.org",
-        port: "",
-      },
-    ],
+    unoptimized: true,
   },
 };
 
