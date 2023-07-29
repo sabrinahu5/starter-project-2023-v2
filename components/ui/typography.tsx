5183b1db7e86591f52904ff8d6d90a4bf2e0888d
Matthew Su <matthewsu@college.harvard.edu>
2023-07-29 14:57:41 -0400

    reworked typography package to accept native component props

--- components/ui/typography.tsx
+++ components/ui/typography.tsx
@@ -1,30 +1,37 @@
-export function TypographyH1({ children }: { children: string }) {
-  return <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">{children}</h1>;
-}
+import { cn } from "@/lib/utils";
+import React, { type ComponentPropsWithoutRef } from "react";
 
-export function TypographyH2({ children }: { children: string }) {
-  return (
-    <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
-      {children}
-    </h2>
-  );
-}
+// https://stackoverflow.com/questions/54654303/using-a-forwardref-component-with-children-in-typescript
 
-export function TypographyH3({ children }: { children: string }) {
-  return <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">{children}</h3>;
-}
+export const TypographyH1 = React.forwardRef<HTMLHeadingElement, ComponentPropsWithoutRef<"h1">>(({ className,...props }, ref) => (
+  <h1 ref={ref} className={cn("scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl", className)} {...props}/>
+));
+TypographyH1.displayName = "TypographyH1";
 
-export function TypographyH4({ children }: { children: string }) {
-  return <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">{children}</h4>;
-}
+export const TypographyH2 = React.forwardRef<HTMLHeadingElement, ComponentPropsWithoutRef<"h2">>(({ className,...props }, ref) => (
+  <h2 ref={ref} className={cn("scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0", className)} {...props}/>
+));
+TypographyH2.displayName = "TypographyH2";
 
-export function TypographyP({ children }: { children: string }) {
-  return <p className="leading-7 [&:not(:first-child)]:mt-6">{children}</p>;
-}
+export const TypographyH3 = React.forwardRef<HTMLHeadingElement, ComponentPropsWithoutRef<"h3">>(({ className,...props }, ref) => (
+  <h3 ref={ref} className={cn("scroll-m-20 text-2xl font-semibold tracking-tight", className)} {...props}/>
+));
+TypographyH3.displayName = "TypographyH3";
 
-export function TypographyBlockquote({ children }: { children: string }) {
-  return <blockquote className="mt-6 border-l-2 pl-6 italic">{children}</blockquote>;
-}
+export const TypographyH4 = React.forwardRef<HTMLHeadingElement, ComponentPropsWithoutRef<"h4">>(({ className,...props }, ref) => (
+  <h4 ref={ref} className={cn("scroll-m-20 text-xl font-semibold tracking-tight", className)} {...props}/>
+));
+TypographyH4.displayName = "TypographyH4";
+
+export const TypographyP = React.forwardRef<HTMLParagraphElement, ComponentPropsWithoutRef<"p">>(({ className,...props }, ref) => (
+  <p ref={ref} className={cn("leading-7 [&:not(:first-child)]:mt-6", className)} {...props}/>
+));
+TypographyP.displayName = "TypographyP";
+
+export const TypographyBlockquote = React.forwardRef<HTMLQuoteElement, ComponentPropsWithoutRef<"blockquote">>(({ className,...props }, ref) => (
+  <blockquote ref={ref} className={cn("mt-6 border-l-2 pl-6 italic", className)} {...props}/>
+));
+TypographyBlockquote.displayName = "TypographyBlockquote";
 
 /* Typing references:
 https://stackoverflow.com/questions/65361696/arguments-of-same-length-typescript
@@ -80,42 +87,47 @@ export function TypographyList({ items }: { items: string[] }) {
   );
 }
 
-export function TypographyInlineCode({ children }: { children: string }) {
-  return (
-    <code className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold">
-      {children}
-    </code>
-  );
-}
+export const TypographyInlineCode = React.forwardRef<HTMLElement, ComponentPropsWithoutRef<"code">>(({ className,...props }, ref) => (
+  <code ref={ref} className={cn("relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold", className)} {...props}/>
+));
+TypographyInlineCode.displayName = "TypographyInlineCode";
 
-export function TypographyLead({ children }: { children: string }) {
-  return <p className="text-xl text-muted-foreground">{children}</p>;
-}
+export const TypographyLead = React.forwardRef<HTMLParagraphElement, ComponentPropsWithoutRef<"p">>(({ className,...props }, ref) => (
+  <p ref={ref} className={cn("text-xl text-muted-foreground", className)} {...props}/>
+));
+TypographyLead.displayName = "TypographyLead";
 
-export function TypographyLarge({ children }: { children: string }) {
-  return <div className="text-lg font-semibold">{children}</div>;
-}
+export const TypographyLarge = React.forwardRef<HTMLDivElement, ComponentPropsWithoutRef<"div">>(({ className,...props }, ref) => (
+  <div ref={ref} className={cn("text-lg font-semibold", className)} {...props}/>
+));
+TypographyLarge.displayName = "TypographyLarge";
 
-export function TypographySmall({ children }: { children: string }) {
-  return <small className="text-sm font-medium leading-none">{children}</small>;
-}
+export const TypographySmall = React.forwardRef<HTMLElement, ComponentPropsWithoutRef<"small">>(({ className,...props }, ref) => (
+  <small ref={ref} className={cn("text-sm font-medium leading-none", className)} {...props}/>
+));
+TypographySmall.displayName = "TypographySmall";
 
-export function TypographyMuted({ children }: { children: string }) {
-  return <p className="text-sm text-muted-foreground">{children}</p>;
-}
+export const TypographyMuted = React.forwardRef<HTMLParagraphElement, ComponentPropsWithoutRef<"p">>(({ className,...props }, ref) => (
+  <p ref={ref} className={cn("text-sm text-muted-foreground", className)} {...props}/>
+));
+TypographyMuted.displayName = "TypographyMuted";
 
-export function PageHeader1({ children }: { children: string }) {
-  return <h2 className="text-2xl font-bold tracking-tight">{children}</h2>;
-}
+export const PageHeader1 = React.forwardRef<HTMLHeadingElement, ComponentPropsWithoutRef<"h2">>(({ className,...props }, ref) => (
+  <h2 ref={ref} className={cn("text-2xl font-bold tracking-tight", className)} {...props}/>
+));
+PageHeader1.displayName = "PageHeader1";
 
-export function PageSubHeader1({ children }: { children: string }) {
-  return <p className="text-muted-foreground">{children}</p>;
-}
+export const PageSubHeader1 = React.forwardRef<HTMLParagraphElement, ComponentPropsWithoutRef<"p">>(({ className,...props }, ref) => (
+  <p ref={ref} className={cn("text-muted-foreground", className)} {...props}/>
+));
+PageSubHeader1.displayName = "PageSubHeader1";
 
-export function PageHeader2({ children }: { children: string }) {
-  return <h3 className="text-lg font-medium">{children}</h3>;
-}
+export const PageHeader2 = React.forwardRef<HTMLHeadingElement, ComponentPropsWithoutRef<"h3">>(({ className,...props }, ref) => (
+  <h3 ref={ref} className={cn("text-lg font-medium", className)} {...props}/>
+));
+PageHeader2.displayName = "PageHeader2";
 
-export function PageSubHeader2({ children }: { children: string }) {
-  return <p className="text-sm text-muted-foreground">{children}</p>;
-}
+export const PageSubHeader2 = React.forwardRef<HTMLParagraphElement, ComponentPropsWithoutRef<"p">>(({ className,...props }, ref) => (
+  <p ref={ref} className={cn("text-sm text-muted-foreground", className)} {...props}/>
+));
+PageSubHeader2.displayName = "PageSubHeader2";
